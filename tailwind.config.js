a53a616c02d5e164c128855b281edc3c0ff553dd
ubaidawan <malikunknown633@gmail.com>
2023-08-30 18:39:07 +0500

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
